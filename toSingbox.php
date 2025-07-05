e9f8fa574ac40363db0f159e5702abf624233695
asemanvpn <eskymobiletm@gmail.com>
2025-07-05 19:35:59 +0330

    Update toSingbox.php

--- toSingbox.php
+++ toSingbox.php
@@ -370,7 +370,7 @@ $directoryOfFiles = [
 ];
 
 foreach ($directoryOfFiles as $directory) {
-    $configsName = "TVC | " . explode("/", $directory)[3];
+    $configsName = "AsemanVPN | " . explode("/", $directory)[3];
     $configsData = file_get_contents($directory);
     $convertionResult = processConvertion($configsData, $configsName);
     file_put_contents("subscriptions/singbox/" . explode("/", $directory)[3] . ".json", $convertionResult);
